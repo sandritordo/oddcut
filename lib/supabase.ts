73e41693dcd60811b500337c344339799a9b4313
sandritordo <sandritordo@gmail.com>
2026-04-10 16:47:16 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
-
 import { createClient } from "@supabase/supabase-js";
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-const service = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
-export const supabase = url && anon ? createClient(url, anon) : null;
-export const supabaseAdmin = url && service ? createClient(url, service) : null;
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
